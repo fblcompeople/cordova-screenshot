54f8c28aefed8bc425c510a9e34f75c15c5e7639
rmu <roman.mueller@compeople.de>
2017-12-18 15:00:35 +0100

    reformat code

--- src/windows/screenshot-proxy.js
+++ src/windows/screenshot-proxy.js
@@ -1,18 +1,18 @@
-cordova.commandProxy.add("Screenshot",{
-    saveScreenshot:function(successCallback,errorCallback,input) {
-		errorCallback("Not implemented.");
+cordova.commandProxy.add("Screenshot", {
+    saveScreenshot: function (successCallback, errorCallback, input) {
+        errorCallback("Not implemented.");
     },
-	getScreenshotAsURI:function(successCallback,errorCallback,input) {
+    getScreenshotAsURI: function (successCallback, errorCallback, input) {
         var inst = new ScreenshotRuntimeComponent.ScreenshotPluginRT()
         var res = inst.screenshotAsync()
-        res.then(function(screenshotImageAsBase64){
-			if (screenshotImageAsBase64.indexOf("Error!") > -1) {
-				errorCallback("Error => " + screenshotImageAsBase64);
-			} else {
-				successCallback({"URI": "data:image/png;base64,"+ screenshotImageAsBase64});
-			}
+        res.then(function (screenshotImageAsBase64) {
+            if (screenshotImageAsBase64.indexOf("Error!") > -1) {
+                errorCallback("Error => " + screenshotImageAsBase64);
+            } else {
+                successCallback({ "URI": "data:image/png;base64," + screenshotImageAsBase64 });
+            }
         })
-        
-        
+
+
     }
 });
